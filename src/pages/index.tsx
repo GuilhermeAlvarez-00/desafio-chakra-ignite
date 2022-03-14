97b8fc739cc4e23b8b95eb5fa0668f159e7f0d97
GuilhermeAlvarez-00 <gui06barboza@gmail.com>
2022-03-14 10:40:19 -0300

    criando sessao home-hero

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,80 @@
 import type { NextPage } from 'next'
-import { Flex } from '@chakra-ui/react'
+import {
+  Flex,
+  Box,
+  Heading,
+  Text,
+  Stack,
+  Image,
+  useBreakpointValue,
+} from '@chakra-ui/react'
 import { Header } from '../components/Header'
 
 const Home: NextPage = () => {
+  const isWideVersion = useBreakpointValue({
+    base: false,
+    md: true,
+    lg: true,
+  })
+
+  console.log(isWideVersion)
+
   return (
-    <Flex as="main" w="100%">
+    <>
       <Header />
-    </Flex>
+      <Flex as="main" w="100%">
+        <Box
+          w="100%"
+          h={['auto', 435, 335]}
+          bgImage="/images/background.png"
+          bgPosition="center"
+          bgRepeat="no-repeat"
+          bgSize="cover"
+          display="flex"
+          alignItems="center"
+          px="4"
+          py={['7', '0']}
+        >
+          <Flex
+            w="100%"
+            maxW={1160}
+            mx="auto"
+            align="center"
+            justify="space-between"
+            flexDir={['column', 'column', 'column', 'row']}
+          >
+            <Stack spacing="5">
+              <Heading
+                color="gray.50"
+                fontSize={['20', '32']}
+                fontWeight="medium"
+                lineHeight="54px"
+              >
+                5 Continentes, <br /> infinitas possibilidades.
+              </Heading>
+              <Text
+                color="gray.100"
+                fontSize={['14', '20']}
+                fontWeight="normal"
+                w="100%"
+                maxW={418}
+              >
+                Chegou a hora de tirar do papel a viagem que você sempre sonhou.{' '}
+              </Text>
+            </Stack>
+
+            {isWideVersion && (
+              <Image
+                src="/images/airplane.svg"
+                alt="Avião nas nuvens"
+                mb="-100px"
+                w={[320, 340, 340, 417]}
+              />
+            )}
+          </Flex>
+        </Box>
+      </Flex>
+    </>
   )
 }
 
