552bfe9ea910081aa5af236defd821f89a0adacf
GuilhermeAlvarez-00 <gui06barboza@gmail.com>
2022-03-14 09:01:41 -0300

    configurando o theme

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,15 @@
 import type { NextPage } from 'next'
+import { Text } from '@chakra-ui/react'
 
 const Home: NextPage = () => {
-  return <h2>Hello World</h2>
+  return (
+    <>
+      <Text color="yellow.500">OI</Text>
+      <Text>OI</Text>
+      <Text color="gray.50">OI</Text>
+      <Text color="gray.500">OI</Text>
+    </>
+  )
 }
 
 export default Home
---
+++ src/styles/theme.ts
@@ -0,0 +1,27 @@
+import { extendTheme } from '@chakra-ui/react'
+
+export const theme = extendTheme({
+  colors: {
+    yellow: {
+      '500': '#FFBA08',
+    },
+    gray: {
+      '50': '#F5F8FA',
+      '100': '#DADADA',
+      '500': '#999999',
+      '600': '#47585B',
+    },
+  },
+  fonts: {
+    heading: 'Poppins',
+    body: 'Poppins',
+  },
+  styles: {
+    global: {
+      body: {
+        bg: 'gray.100',
+        color: 'gray.600',
+      },
+    },
+  },
+})
