84919aef06eb6ef7acecd7d40804f82ae19de2a1
GuilhermeAlvarez-00 <gui06barboza@gmail.com>
2022-03-15 11:18:30 -0300

    componente homebanner

---
+++ src/components/HomeBanner/index.tsx
@@ -0,0 +1,72 @@
+import {
+  Flex,
+  Box,
+  Heading,
+  Text,
+  Stack,
+  Image,
+  useBreakpointValue,
+} from '@chakra-ui/react'
+
+export function HomeBanner() {
+  const isWideVersion = useBreakpointValue({
+    base: false,
+    md: true,
+    lg: true,
+  })
+
+  return (
+    <Flex as="section" w="100%">
+      <Box
+        w="100%"
+        h={['auto', 435, 335]}
+        bgImage="/images/background.png"
+        bgPosition="center"
+        bgRepeat="no-repeat"
+        bgSize="cover"
+        display="flex"
+        alignItems="center"
+        px="4"
+        py={['7', '0']}
+      >
+        <Flex
+          w="100%"
+          maxW={1160}
+          mx="auto"
+          align="center"
+          justify="space-between"
+          flexDir={['column', 'column', 'column', 'row']}
+        >
+          <Stack spacing="5">
+            <Heading
+              color="gray.50"
+              fontSize={['20', '32']}
+              fontWeight="medium"
+              lineHeight="54px"
+            >
+              5 Continentes, <br /> infinitas possibilidades.
+            </Heading>
+            <Text
+              color="gray.100"
+              fontSize={['14', '20']}
+              fontWeight="normal"
+              w="100%"
+              maxW={418}
+            >
+              Chegou a hora de tirar do papel a viagem que você sempre sonhou.{' '}
+            </Text>
+          </Stack>
+
+          {isWideVersion && (
+            <Image
+              src="/images/airplane.svg"
+              alt="Avião nas nuvens"
+              mb="-100px"
+              w={[320, 340, 340, 417]}
+            />
+          )}
+        </Flex>
+      </Box>
+    </Flex>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,6 +13,7 @@ import {
 import { Header } from '../components/Header'
 import { TravelTypes } from '../components/TravelTypes'
 import { Slide } from '../components/Slide'
+import { HomeBanner } from '../components/HomeBanner'
 
 const Home: NextPage = () => {
   const isWideVersion = useBreakpointValue({
@@ -27,59 +28,7 @@ const Home: NextPage = () => {
     <>
       <Header />
       <Flex as="main" w="100%" flexDir="column">
-        <Flex as="section" w="100%">
-          <Box
-            w="100%"
-            h={['auto', 435, 335]}
-            bgImage="/images/background.png"
-            bgPosition="center"
-            bgRepeat="no-repeat"
-            bgSize="cover"
-            display="flex"
-            alignItems="center"
-            px="4"
-            py={['7', '0']}
-          >
-            <Flex
-              w="100%"
-              maxW={1160}
-              mx="auto"
-              align="center"
-              justify="space-between"
-              flexDir={['column', 'column', 'column', 'row']}
-            >
-              <Stack spacing="5">
-                <Heading
-                  color="gray.50"
-                  fontSize={['20', '32']}
-                  fontWeight="medium"
-                  lineHeight="54px"
-                >
-                  5 Continentes, <br /> infinitas possibilidades.
-                </Heading>
-                <Text
-                  color="gray.100"
-                  fontSize={['14', '20']}
-                  fontWeight="normal"
-                  w="100%"
-                  maxW={418}
-                >
-                  Chegou a hora de tirar do papel a viagem que você sempre
-                  sonhou.{' '}
-                </Text>
-              </Stack>
-
-              {isWideVersion && (
-                <Image
-                  src="/images/airplane.svg"
-                  alt="Avião nas nuvens"
-                  mb="-100px"
-                  w={[320, 340, 340, 417]}
-                />
-              )}
-            </Flex>
-          </Box>
-        </Flex>
+        <HomeBanner />
 
         <Flex as="section" w="100%" px="4">
           <Flex
