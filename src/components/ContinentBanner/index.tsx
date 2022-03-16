1a5232aa3f15f0b852d0e6e31889cd70a731f162
GuilhermeAlvarez-00 <gui06barboza@gmail.com>
2022-03-16 14:42:54 -0300

    melhorando responsivo do banner continent

--- src/components/ContinentBanner/index.tsx
+++ src/components/ContinentBanner/index.tsx
@@ -4,7 +4,7 @@ export function ContinentBanner() {
   return (
     <Flex
       w="100%"
-      h="500px"
+      h={['150px', '250px', '350px', '500px']}
       bgImage="https://images.unsplash.com/photo-1499856871958-5b9627545d1a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=820&q=80"
       bgPosition="center"
       bgRepeat="no-repeat"
@@ -13,11 +13,11 @@ export function ContinentBanner() {
       <Flex
         w="100%"
         h="100%"
-        align="flex-end"
-        justify={['center', 'flex-start', 'flex-start']}
+        align={['center', 'center', 'flex-end']}
+        justify={['center', 'center', 'flex-start']}
         bgGradient="linear(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7))"
       >
-        <Heading color="white" ml={['0', '100px', '140px']} mb="60px">
+        <Heading color="white" ml={['0', '0', '140px']} mb={['0', '0', '60px']}>
           Europa
         </Heading>
       </Flex>
