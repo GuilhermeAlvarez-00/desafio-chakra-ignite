0af9caa6bd93956bfdb7f91b124dc72b63aa113f
GuilhermeAlvarez-00 <gui06barboza@gmail.com>
2022-03-16 14:58:15 -0300

    fazendo menu mobile

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,38 @@
-import { Flex, Image } from '@chakra-ui/react'
+import { Flex, Icon, Image, useBreakpointValue } from '@chakra-ui/react'
+import { ChevronLeftIcon } from '@chakra-ui/icons'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 export function Header() {
+  const isMobile = useBreakpointValue({
+    base: true,
+    md: false,
+  })
+
+  const { asPath } = useRouter()
+
   return (
     <Flex as="header" w="100%" h={100} align="center" justify="center">
-      <Image src="/images/logo.svg" alt="Logo" w={184} />
+      <Flex
+        w="100%"
+        maxW="1160px"
+        align="center"
+        justify="center"
+        position="relative"
+      >
+        {isMobile && asPath !== '/' && (
+          <Link href="/" passHref>
+            <Icon
+              as={ChevronLeftIcon}
+              position="absolute"
+              left="0"
+              w={8}
+              h={8}
+            />
+          </Link>
+        )}
+        <Image src="/images/logo.svg" alt="Logo" w={184} />
+      </Flex>
     </Flex>
   )
 }
